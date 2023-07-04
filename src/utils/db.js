acd5d63f77d8a02128b00b736bf3082046ef3293
kalam <111223748+bagus491@users.noreply.github.com>
2023-07-04 19:29:55 +0700

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,7 @@
 const mongose = require('mongoose')
 const{MongoClient} = require('mongodb')
 
-const url = 'mongodb://bagus:12345@ac-aax6ywu-shard-00-00.s8edxgg.mongodb.net:27017,ac-aax6ywu-shard-00-01.s8edxgg.mongodb.net:27017,ac-aax6ywu-shard-00-02.s8edxgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-fvxcnp-shard-0&authSource=admin&retryWrites=true&w=majority'
+const url = 'mongodb://bagus:<password>@ac-aax6ywu-shard-00-00.s8edxgg.mongodb.net:27017,ac-aax6ywu-shard-00-01.s8edxgg.mongodb.net:27017,ac-aax6ywu-shard-00-02.s8edxgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-fvxcnp-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 mongose.connect(url,{
     useNewUrlParser: true,
